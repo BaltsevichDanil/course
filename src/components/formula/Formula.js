8a104b304f5d4711e33740923baeafa5494ddfe6
Danil Baltsevich <gnngr333@gmail.com>
2020-12-30 15:19:34 +0700

    add:   method removeDomListeners in DomListener.js   method removeListener in dom.js

--- src/components/formula/Formula.js
+++ src/components/formula/Formula.js
@@ -3,6 +3,13 @@ import {ExcelComponent} from '@core/ExcelComponent'
 export class Formula extends ExcelComponent {
   static className = 'excel__formula'
 
+  constructor($root) {
+    super($root, {
+      name: 'Formula',
+      listeners: ['input', 'click']
+    })
+  }
+
   toHTML() {
     return `
       <div class="info">
@@ -10,5 +17,14 @@ export class Formula extends ExcelComponent {
       </div>
       <div class="input" contenteditable="true" spellcheck="false"></div>
     `
+  }
+
+  onInput(event) {
+    console.log(this.$root)
+    console.log('Formula Input: ', event)
+  }
+
+  onClick() {
+    console.log('click')
   }
 }
